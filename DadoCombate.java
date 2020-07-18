ee397425f5610855148395c3389afdcb5ebd9873
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 14:54:48 -0300

    Update DadoCombate.java

--- DadoCombate.java
+++ DadoCombate.java
@@ -5,11 +5,13 @@ import java.util.*;
  * 
  */
 public class DadoCombate extends Dado {
-
-    /**
+    
+    private Lado[] faces;
+    /** 
      * Default constructor
      */
     public DadoCombate() {
+        faces = new Lado[] {ESCUDO_MONSTRO, ESCUDO_HEROI, ESCUDO_HEROI, CAVEIRA, CAVEIRA, CAVEIRA};
     }
 
 
@@ -18,7 +20,9 @@ public class DadoCombate extends Dado {
     /**
      * 
      */
-    public void jogar() {
+    public Lado jogar() {
+        Random resultado = new Random[];
+        return faces[resultado.nextInt(6) + 1]; 
         // TODO implement here
     }
 
