649323d23225a71f75c4e67f27fec7183326e23e
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 15:18:54 -0300

    Update Anao.java

--- Anao.java
+++ Anao.java
@@ -1,15 +1,23 @@
 
 import java.util.*;
-//ooi tudo bom?
 /**
  * 
  */
 public class Anao extends Heroi {
-
+    private static int VIDA_INICIAL = 7;
+    private static int INT_INICIAL = 3;
+    private static int ATQ_INICIAL = 2;
+    private static int DFS_INICIAL = 2;
+    
     /**
      * Default constructor
      */
-    public Anao() {
+    public Anao(String nome) {
+        super(nome);
+        this.vida = VIDA_INICIAL;
+        this.inteligencia = INT_INICIAL;
+        this.bonusAtaque = ATQ_INICIAL;
+        this.bonusDefesa = DFS_INICIAL;
     }
 
 }
