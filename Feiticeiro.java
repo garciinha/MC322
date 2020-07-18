5f6118c20d721ada9f6b86ef6d8e45850e14259c
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 15:22:39 -0300

    Update Feiticeiro.java

--- Feiticeiro.java
+++ Feiticeiro.java
@@ -5,11 +5,19 @@ import java.util.*;
  * 
  */
 public class Feiticeiro extends Heroi {
-
+    private static int VIDA_INICIAL = 4;
+    private static int INT_INICIAL = 6;
+    private static int ATQ_INICIAL = 1;
+    private static int DFS_INICIAL = 2;
     /**
      * Default constructor
      */
-    public Feiticeiro() {
+    public Feiticeiro(String nome) {
+        super(nome);
+        this.vida = VIDA_INICIAL;
+        this.inteligencia = INT_INICIAL;
+        this.bonusAtaque = ATQ_INICIAL;
+        this.bonusDefesa = DFS_INICIAL;
     }
 
 }
