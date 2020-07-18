43aa451942578629486e73dfaca0d77d585974e6
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 15:20:43 -0300

    Update Elfo.java

--- Elfo.java
+++ Elfo.java
@@ -5,12 +5,18 @@ import java.util.*;
  * 
  */
 public class Elfo extends Heroi {
-
-    /**
-     * Default constructor
-     */
-    public Elfo() {
-      
+    
+    private static int VIDA_INICIAL = 6;
+    private static int INT_INICIAL = 4;
+    private static int ATQ_INICIAL = 2;
+    private static int ATQ_INICIAL = 2;
+    
+    public Elfo(String nome) {
+        super(nome);
+        this.vida = VIDA_INICIAL;
+        this.inteligencia = INT_INICIAL;
+        this.bonusAtaque = ATQ_INICIAL;
+        this.bonusDefesa = DFS_INICIAL;
     }
 
 }
