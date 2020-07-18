78c02f7d144adb659f3c32ed4be16594ffe2f396
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 17:37:52 -0300

    ad

--- Anao.java
+++ Anao.java
@@ -1,14 +1,16 @@
 
 import java.util.*;
 /**
- * 
+ *
  */
 public class Anao extends Heroi {
     private static int VIDA_INICIAL = 7;
     private static int INT_INICIAL = 3;
     private static int ATQ_INICIAL = 2;
     private static int DFS_INICIAL = 2;
-    
+
+    //ttt
+
     /**
      * Default constructor
      */
