8e409e65bf582bd9ce75ad357a4d5f1334f8b850
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-18 15:17:09 -0300

    Update Barbaro.java

--- Barbaro.java
+++ Barbaro.java
@@ -5,11 +5,18 @@ import java.util.*;
  * 
  */
 public class Barbaro extends Barbaro {
-
-    /**
-     * Default constructor
-     */
-    public Barbaro() {
+    
+    private static int VIDA_INICIAL = 8;
+    private static int INT_INICIAL = 2;
+    private static int ATQ_INICIAL = 3;
+    private static int ATQ_INICIAL = 2;
+    
+    public Barbaro(String nome) {
+        super(nome);
+        this.vida = VIDA_INICIAL;
+        this.inteligencia = INT_INICIAL;
+        this.bonusAtaque = ATQ_INICIAL;
+        this.bonusDefesa = DFS_INICIAL;
     }
 
 }
