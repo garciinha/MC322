fa9caa30d075f366065e9e270bcbc1affd96d9c6
garciinha <68205063+garciinha@users.noreply.github.com>
2020-07-19 04:13:43 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,16 +5,22 @@ import java.util.*;
  * 
  */
 public class Game {
-
+    private boolean exitSelected;
     /**
      * Default constructor
      */
     public Game() {
     }
-
-
-
-
+    public void start() {
+        exit = false;
+        System.out.println("Game started!");
+        while(!exit) {
+            drawBoard();
+            lerInput();
+            updateBoard();
+        }
+        System.out.println("Game terminated. Bye!");
+    }
 
     /**
      * 
