2c012507686b8da12a0eef7f63796cdc3098f425
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-23 16:51:21 -0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -19,43 +19,45 @@ public class Game {
     }
 
     public void lerInput() {
-        Scanner keyboard = new Scanner ( System . in ) ;
+        Scanner keyboard = new Scanner ( System.in ) ;
         boolean acaoDisponivel = true;
-        boolean podeAndar =true;
+        boolean podeAndar = true;
         boolean  jogadorNaoDesistiu = true;
         while (jogadorNaoDesistiu && (acaoDisponivel || podeAndar) ) {
-            System.out.print (" Digite a direcao que deseja andar, acao ou desistir (caso queira passar para a proxima rodada) : ") ;
-            String command = keyboard . nextLine () ;
-            if ( command . compareTo ("desistir") == 0) {
-            jogadorNaoDesistiu = false ;
-            } else if ( command . compareTo ("w") == 0) {
+            System.out.print(" Digite a direcao que deseja andar (wasd), acao (acao) ou desistir(caso queira passar para a proxima rodada) (pass) : ") ;
+            String command = keyboard.nextLine() ;
+            if ( command.compareTo ("pass") == 0) {
+                jogadorNaoDesistiu = false ;
+            } else if ( command.compareTo ("w") == 0) {
                 //funcao de andar
                 //verifica se ainda pode andar, caso nao modifica o podeAndar parar false;
-            } else if ( command . compareTo ("a") == 0) {
+            } else if ( command.compareTo ("a") == 0) {
                 //funcao de andar
                 //verifica se ainda pode andar, caso nao modifica o podeAndar parar false;
-            } else if ( command . compareTo ("s") == 0) {
+            } else if ( command.compareTo ("s") == 0) {
                 //funcao de andar
                 //verifica se ainda pode andar, caso nao modifica o podeAndar parar false;
-            } else if ( command . compareTo ("d") == 0) {
+            } else if ( command.compareTo ("d") == 0) {
                 //funcao de andar
                 //verifica se ainda pode andar, caso nao modifica o podeAndar parar false;
-            } else if ( command . compareTo ("acao") == 0) {
-                System.out.print (" Escolha uma acao entre : atacar, usar magia, tomar pocao cura, tomar pocao forca, procurar armadilhas");
-                acaoDisponivel= false;
-                if ( command . compareTo ("atacar") == 0) {
+            } else if ( command.compareTo ("acao") == 0) {
+                if(!acaoDisponivel) {
+                    System.out.println("Voce nao pode realizar uma acao agora.");
+                else {
+                System.out.println("Escolha uma acao entre : atacar (a), usar magia (s), tomar pocao (q), procurar (f)");
+                acaoDisponivel = false;
+                if (command.compareTo ("a") == 0) {
                 //chama a funcao de ataque 
-                } else if ( command . compareTo ("usar magia") == 0) {
+                } 
+                else if ( command.compareTo ("s") == 0) {
                     //chama a funcao de usar magia
-                } else if ( command . compareTo ("tomar pocao cura") == 0) {
+                } 
+                else if ( command.compareTo ("q") == 0) {
                     //chama a funcao da pocao de cura 
-                } else if ( command . compareTo ("tomar pocao forca") == 0) {
-                    //chama a funcao da pocao da forca
-                } else if ( command . compareTo ("procurar armadilhas") == 0) {
+                } 
+                else if ( command.compareTo ("f") == 0) {
                     // chama a funcao de olhar armadilhas no mapa
-                }else if ( command . compareTo ("procurar tesouros") == 0) {
-                    // chama a funcao de procurar tesouros no mapa
-                }
+                } 
         }
     }
 
